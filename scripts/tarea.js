fa5727cb95b96c8e58af3309c404e29117dc339a
Alvaro <alvarodromantligero@alumnos.tuniversformacion.es>
2023-11-27 08:51:15 +0100

    Elaboramos la clase tarea

---
+++ scripts/tarea.js
@@ -0,0 +1,11 @@
+export class Tarea {
+  constructor(id, descripcion){
+    this.id = id;
+    this.descripcion = descripcion;
+  }
+
+  editar(descripcion){
+    this.descripcion = descripcion;
+  }
+
+}
